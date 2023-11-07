d56927776aa0a315124558e925c18cc25bc62158
tsyganno <tsyganlee@gmail.com>
2023-11-07 20:19:37 +0800

    final

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from PIL import Image
-import pillow_heif
 import os
+import moviepy.editor as moviepy
+import pillow_heif
+from PIL import Image
+
 
 SourceFolder = "C:/Users/tsyga/OneDrive/Изображения/original"
 TargetFolder = "C:/Users/tsyga/OneDrive/Изображения/final"
@@ -9,6 +11,17 @@ pillow_heif.register_heif_opener()
 
 
 for file in os.listdir(SourceFolder):
-    img = Image.open(f'C:/Users/tsyga/OneDrive/Изображения/original/{file}')
-    img.save(f'C:/Users/tsyga/OneDrive/Изображения/final/{file.split(".")[0]}.jpeg', format('jpeg'))
+    if 'HEIC' in file:
+        img = Image.open(f'C:/Users/tsyga/OneDrive/Изображения/original/{file}')
+        img.save(f'C:/Users/tsyga/OneDrive/Изображения/final/{file.split(".")[0]}.jpeg', format('jpeg'))
+    elif 'MOV' in file:
+        clip = moviepy.VideoFileClip(f'C:/Users/tsyga/OneDrive/Изображения/original/{file}')
+        if clip.rotation == 90:
+            clip = clip.resize(clip.size[::-1])
+            clip.rotation = 0
+            clip.write_videofile(f'C:/Users/tsyga/OneDrive/Изображения/final/{file.split(".")[0]}.mp4')
+        else:
+            clip.write_videofile(f'C:/Users/tsyga/OneDrive/Изображения/final/{file.split(".")[0]}.mp4')
+    else:
+        print(file)
 
